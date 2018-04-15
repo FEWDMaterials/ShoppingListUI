4a41a007e9b85e3d76ff8b89bc5fa95cf5975d0b
Nazario A. Ayala <nazario@niztech.com>
2018-04-15 13:12:01 -0700

    LINT

--- javascript/shoppinglist.js
+++ javascript/shoppinglist.js
@@ -4,22 +4,22 @@ const getShoppingList = () => {
 		addToList: (item) => {
 			ShoppingList.list.push(item);
 
-			return ShoppingList.list;	
+			return ShoppingList.list;
 		},
 		displayList: () => {
 			let str = '';
 			for (let i = 0; i < ShoppingList.list.length; i++) {
-				str += (i+1) + '. ' + ShoppingList.list[i] + '\n';
+				str += (i + 1) + '. ' + ShoppingList.list[i] + '\n';
 			}
 			return str;
 		},
 		countItemsInList: () => {
 			return ShoppingList.list.length;
 		}
-	};	
+	};
 
 	return ShoppingList;
-}
+};
 
 const ctown = getShoppingList();
 const wholefoods = getShoppingList();
@@ -27,12 +27,12 @@ const wholefoods = getShoppingList();
 ctown.addToList('mangoes');
 ctown.addToList('coffee');
 
-wholefoods.addToList('steak') 
-wholefoods.addToList('potatoes')
+wholefoods.addToList('steak');
+wholefoods.addToList('potatoes');
 
-console.log(wholefoods.countItemsInList())
+console.log(wholefoods.countItemsInList());
 
-console.log('wholefoods fam')
-console.log(wholefoods.displayList())
-console.log('ctown fam')
-console.log(ctown.displayList())
+console.log('wholefoods fam');
+console.log(wholefoods.displayList());
+console.log('ctown fam');
+console.log(ctown.displayList());
