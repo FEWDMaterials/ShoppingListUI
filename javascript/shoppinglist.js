2c39b98da5217d7445db27774cdea7da017291f6
Nazario A. Ayala <nazario@niztech.com>
2018-04-15 14:06:59 -0700

    Wrapped JS in IIFE

--- javascript/shoppinglist.js
+++ javascript/shoppinglist.js
@@ -1,45 +1,47 @@
-const getShoppingList = (name) => {
-	const ShoppingList = {
-		list: [],
-		listName: '',
-		addToList: (item) => {
-			ShoppingList.list.push(item);
+(function () {
+	const getShoppingList = (name) => {
+		const ShoppingList = {
+			list: [],
+			listName: '',
+			addToList: (item) => {
+				ShoppingList.list.push(item);
 
-			return ShoppingList.list;
-		},
-		displayList: () => {
-			let str = '';
-			for (let i = 0; i < ShoppingList.list.length; i++) {
-				str += (i + 1) + '. ' + ShoppingList.list[i] + '\n';
+				return ShoppingList.list;
+			},
+			displayList: () => {
+				let str = '';
+				for (let i = 0; i < ShoppingList.list.length; i++) {
+					str += (i + 1) + '. ' + ShoppingList.list[i] + '\n';
+				}
+				return str;
+			},
+			countItemsInList: () => {
+				return ShoppingList.list.length;
+			},
+
+			setListName: (name) => {
+				ShoppingList.listName = name;
 			}
-			return str;
-		},
-		countItemsInList: () => {
-			return ShoppingList.list.length;
-		},
+		};
 
-		setListName: (name) => {
-			ShoppingList.listName = name;
-		}
+		ShoppingList.setListName(name);
+
+		return ShoppingList;
 	};
 
-	ShoppingList.setListName(name);
+	const ctown = getShoppingList('CTown');
+	const wholefoods = getShoppingList('Whole Foods');
 
-	return ShoppingList;
-};
+	ctown.addToList('mangoes');
+	ctown.addToList('coffee');
 
-const ctown = getShoppingList('CTown');
-const wholefoods = getShoppingList('Whole Foods');
+	wholefoods.addToList('steak');
+	wholefoods.addToList('potatoes');
 
-ctown.addToList('mangoes');
-ctown.addToList('coffee');
+	console.log(wholefoods.countItemsInList());
 
-wholefoods.addToList('steak');
-wholefoods.addToList('potatoes');
-
-console.log(wholefoods.countItemsInList());
-
-console.log('wholefoods fam');
-console.log(wholefoods.displayList());
-console.log('ctown fam');
-console.log(ctown.displayList());
+	console.log('wholefoods fam');
+	console.log(wholefoods.displayList());
+	console.log('ctown fam');
+	console.log(ctown.displayList());
+})();
