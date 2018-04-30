6e513217c56e40def9b235a0318df436221092ae
Nazario A. Ayala <nazario@niztech.com>
2018-04-29 22:47:22 -0700

    Refactored code to use objects with rendfer fucntions.

--- javascript/shoppinglist.js
+++ javascript/shoppinglist.js
@@ -5,75 +5,73 @@
 	const btnAdd = document.querySelector('.inp-actions .btn-add');
 	const btnReset = document.querySelector('.inp-actions .btn-reset');
 
-	const listBody = document.querySelector('.lst-items');
 	const listTitleElm = document.querySelector('.list .list-title');
 	const listCountElm = document.querySelector('.list .list-item-count');
 
-	const tplItemRow = document.querySelector('#tpl-item-row');
 
-	const shoppingListItem = (name, qty) => {
+	const ListObj = (function () {
+		const _listBody = document.querySelector('.lst-items');
+		const _items = [];
+		let listName = '';
 
-		_render: function () {
-
-		}
-	};
-
-	const getShoppingList = (name) => {
-		const ShoppingList = {
-			list: [],
-			listName: '',
-			addToList: (item) => {
-				ShoppingList.list.push(item);
-
-				return ShoppingList.list;
-			},
-			displayList: () => {
-				listBody.innerHTML = '';
-				for (let i = 0; i < ShoppingList.list.length; i++) {
-					const row = tplItemRow.content.cloneNode(true);
-					row.querySelector('.lst-item').dataset.entryId = i;
-					row.querySelector('.item-name').innerHTML = ShoppingList.list[i];
-					listBody.appendChild(row);
-				}
-			},
-			countItemsInList: () => {
-				return ShoppingList.list.length;
-			},
-
-			setListName: (name) => {
-				ShoppingList.listName = name;
-			}
+		const _render = () => {
+			_listBody.innerHTML = '';
+			_items.forEach(item => {
+				_listBody.appendChild(item.render());
+			});
 		};
 
-		ShoppingList.setListName(name);
+		return {
+			setListName: (name) => {
+				listName = name;
+			},
 
-		return ShoppingList;
+			addItem: (item) => {
+				_items.push(item);
+				_render();
+			},
+		}
+	})();
+
+
+	const Item = {
+		_amount: 1,
+		_name: '',
+
+		_render: function () {
+			const _elemHandel = document.importNode(document.querySelector('#tpl-item-row').content, true);
+			_elemHandel.querySelector('.item-qty').innerHTML = this._amount;
+			_elemHandel.querySelector('.item-name').innerHTML = this._name;
+			return _elemHandel;
+		},
+
+		create: function (amount, name) {
+			const item = Object.create(this);
+			item._name = name;
+			item._amount = amount;
+			item.render = item._render;
+			return item;
+		},
 	};
+
+	ListObj.setListName('Test List');
 
 	const actionAddItemToList = () => {
 		const value = inpItemNameElm.value.trim();
 		const qty = parseFloat(inpQtyElm.value.trim());
 
-		if(value.length !== 0) {
-			list.addToList(value);
+		if (value.length !== 0) {
+			ListObj.addItem(Item.create(qty, value));
 			inpItemNameElm.value = '';
+			inpQtyElm.value = '';
 		}
-		list.displayList();
-	};
-
-	const actionEditItemInList = (rowId) => {
-		// Copy data name into input.
-		// Copy data id into hidden.
 	};
 
 	btnAdd.addEventListener('click', actionAddItemToList);
-
 	document.addEventListener('keyup', (event) => {
 		if (event.keyCode === 13) {
 			actionAddItemToList;
 		}
 	});
 
-	const list = getShoppingList('CTown');
-	list.displayList();
 })();
