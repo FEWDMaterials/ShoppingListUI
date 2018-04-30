c1af3940fbc48008fabe707f7002daa871dea660
Nazario A. Ayala <nazario@niztech.com>
2018-04-29 23:44:31 -0700

    Added totals

--- javascript/shoppinglist.js
+++ javascript/shoppinglist.js
@@ -1,24 +1,35 @@
 (function () {
 	const inpQtyElm = document.querySelector('.inp-qty .inp');
-	const inpItemNameElm = document.querySelector('.inp-item-name .inp');
+	const inpNameElm = document.querySelector('.inp-item-name .inp');
+	const inpPriceElm = document.querySelector('.inp-item-price .inp');
+
 	const inpEditIdElm = document.querySelector('#edit-row-id');
 	const btnAdd = document.querySelector('.inp-actions .btn-add');
-	const btnReset = document.querySelector('.inp-actions .btn-reset');
 
 	const listTitleElm = document.querySelector('.list .list-title');
-	const listCountElm = document.querySelector('.list .list-item-count');
 
 
 	const ListObj = (function () {
 		const _listBody = document.querySelector('.lst-items');
+		const totalQty = document.querySelector('.lst-totals .total-qty');
+		const totalPrice = document.querySelector('.lst-totals .total-price');
+
 		const _items = [];
 		let listName = '';
 
 		const _render = () => {
+			let _qtyTotal = 0;
+			let _priceTotal = 0;
 			_listBody.innerHTML = '';
+
 			_items.forEach(item => {
+				_qtyTotal = _qtyTotal + item._qty;
+				_priceTotal = _priceTotal + item._price;
 				_listBody.appendChild(item.render());
 			});
+
+			totalQty.innerHTML = _qtyTotal;
+			totalPrice.innerHTML = _priceTotal.toFixed(2);
 		};
 
 		return {
@@ -35,20 +46,23 @@
 
 
 	const Item = {
-		_amount: 1,
+		_qty: 1,
 		_name: '',
+		_price: 0,
 
 		_render: function () {
 			const _elemHandel = document.importNode(document.querySelector('#tpl-item-row').content, true);
-			_elemHandel.querySelector('.item-qty').innerHTML = this._amount;
+			_elemHandel.querySelector('.item-qty').innerHTML = this._qty;
 			_elemHandel.querySelector('.item-name').innerHTML = this._name;
+			_elemHandel.querySelector('.item-price').innerHTML = this._price.toFixed(2);
 			return _elemHandel;
 		},
 
-		create: function (amount, name) {
+		create: function (qty, name, price) {
 			const item = Object.create(this);
+			item._qty = qty;
 			item._name = name;
-			item._amount = amount;
+			item._price = price;
 			item.render = item._render;
 			return item;
 		},
@@ -57,13 +71,15 @@
 	ListObj.setListName('Test List');
 
 	const actionAddItemToList = () => {
-		const value = inpItemNameElm.value.trim();
 		const qty = parseFloat(inpQtyElm.value.trim());
+		const name = inpNameElm.value.trim();
+		const price = parseFloat(inpPriceElm.value.trim());
 
-		if (value.length !== 0) {
-			ListObj.addItem(Item.create(qty, value));
-			inpItemNameElm.value = '';
+		if (name.length !== 0) {
+			ListObj.addItem(Item.create(qty, name, price));
 			inpQtyElm.value = '';
+			inpNameElm.value = '';
+			inpPriceElm.value = '';
 		}
 	};
 
