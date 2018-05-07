24bbfc06e0d2b17e5ae221208c6b1611463198d2
Tim Hankinson <tim@chronosinteractive.com>
2018-05-06 18:15:38 -0600

    refactor list

--- javascript/main.js
+++ javascript/main.js
@@ -1,45 +1,114 @@
-const newShoppingListItem = (item,price) => ({item, price,})
-const addToShoppingList = (item,list=[]) => list.concat([item]);
-const removeFromShoppingList = (list=[]) => list.slice(0, -1);
-const removeFirstItem = (list=[]) => list.slice(1);
-const removeNthItem = (i,list = []) => {
-  if (i<list.length && i>0 && Number.isInteger(i) == true) {
-    list.splice(i, 1);
+const itemInput = document.querySelector('.js-item-input');
+const itemBtn = document.querySelector('.js-add-btn');
+const listWrap = document.querySelector('.js-list');
+const completedList = document.querySelector('.js-list-completed');
+
+const createItem = (itemName) =>  {
+  const listItem = document.createElement("li");
+  const checkBox = document.createElement("input");
+  const label = document.createElement("label");//label
+  const editInput = document.createElement("input");//text
+  const editButton = document.createElement("button");//edit button
+  const deleteButton = document.createElement("button");//delete button
+
+  label.innerText = itemName;
+  listItem.className = 'list-group-item';
+  checkBox.type="checkbox";
+  editInput.type = 'text';
+  editInput.className = 'js-edit-input'
+  editButton.innerText = 'Edit';
+  editButton.className = 'js-edit btn';
+  deleteButton.innerText = 'X';
+  deleteButton.className = 'js-delete btn btn-danger';
+
+  listItem.appendChild(checkBox)
+  listItem.appendChild(label);
+  listItem.appendChild(editInput);
+  listItem.appendChild(deleteButton);
+  listItem.appendChild(editButton);
+
+  return listItem;
+};
+
+const addItem = () => {
+  const listItem = createItem(itemInput.value);
+  listWrap.appendChild(listItem);
+  bindTaskEvents(listItem, itemCompleted);
+
+  itemInput.value = '';
+
+};
+
+const editItem = function() {
+
+  const listItem = this.parentNode;
+  const editBtn = listItem.querySelector('.js-edit');
+  const editInput = listItem.querySelector('.js-edit-input');
+  const label = listItem.querySelector('label');
+
+  if(listItem.classList.contains('editMode')){
+
+    label.innerText=editInput.value;
+    editBtn.text = 'Save';
   } else {
-    throw new Error('error')
+    editInput.value = label.innerText;
+    editBtn.text = 'Edit';
   }
-  return list;
+
+
+  listItem.classList.toggle('editMode');
 };
 
-const render = () => {
-  let htmlStr = `<tbody id="list-content">`;
-  for(let i = 0; i < shoppingList.length; i++) {
-    htmlStr += `<tr>
-									<td>${(i+1)}</td>
-									<td>${shoppingList[i].item}</td>
-									<td><button type="button" class="btn btn-danger js-remove-btn">X</button></td>
-								</tr>`;
-  }
-  htmlStr += `</tbody>`;
-  document.querySelector('.js-list-table').innerHTML = htmlStr;
+const deleteItem = function() {
+  const listItem = this.parentNode;
+  const ul = listItem.parentNode;
+
+  ul.removeChild(listItem);
+
 };
 
-let shoppingList = [];
 
-const shoppingInput = document.querySelector('.js-item-input');
-shoppingInput.addEventListener('keydown', e => {
+//Mark task completed
+const itemCompleted = function() {
+
+  const listItem = this.parentNode;
+  completedList.appendChild(listItem);
+  bindTaskEvents(listItem, itemIncomplete);
+
+};
+
+
+const itemIncomplete = function() {
+  const listItem = this.parentNode;
+  listWrap.appendChild(listItem);
+  bindTaskEvents(listItem,itemCompleted);
+};
+
+itemBtn.addEventListener('click', addItem);
+itemInput.addEventListener('keydown', e => {
   if (e.keyCode === 13) {
     e.preventDefault();
-
-    const value = e.target.value;
-    const newListItem = newShoppingListItem(value, 0)
-    shoppingList = addToShoppingList(newListItem, shoppingList)
-    e.target.value = "";
-    render();
+    return addItem();
   }
 });
 
-const removeBtn = document.querySelector('.js-remove-btn');
-removeBtn.addEventListener('click', e => {
-    this.removeFromShoppingList(item);
-});
+const bindTaskEvents= (shoppingListItem, checkBoxEventHandler) => {
+
+  const checkBox = shoppingListItem.querySelector("input[type=checkbox]");
+  const editButton = shoppingListItem.querySelector('.js-edit');
+  const deleteButton = shoppingListItem.querySelector('.js-delete');
+
+  editButton.addEventListener('click', editItem);
+  deleteButton.addEventListener('click', deleteItem);
+  checkBox.onchange = checkBoxEventHandler;
+};
+
+// bind events to shopping list
+for (let i = 0; i < listWrap.children.length; i++){
+  bindTaskEvents(listWrap.children[i], itemCompleted());
+}
+
+// bind events to completed list
+for (let i=0; i < completedList.children.length; i++){
+  bindTaskEvents(completedList.children[i], itemIncomplete());
+}
